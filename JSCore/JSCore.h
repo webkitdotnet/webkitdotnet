ce7028690bf519be17c9f5eeb6e889ae8df14f6f
Peter Nelson <peter@peterdn.com>
2010-09-11 18:06:00 +0100

    Added JSCore project
    Signed-off-by: Peter Nelson <peter@peterdn.com>

---
+++ JSCore/JSCore.h
@@ -0,0 +1,15 @@
+// JSCore.h
+
+#pragma once
+
+using namespace System;
+
+namespace WebKit {
+namespace JSCore {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+
+}}
