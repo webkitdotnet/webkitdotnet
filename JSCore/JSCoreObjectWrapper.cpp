7993ce92e800efb61517ee234488ba1116cc155a
Peter Nelson <peter@peterdn.com>
2010-09-23 21:38:38 +0100

    Implemented wrapper_GetProperty, wrapper_SetProperty, added stub for wrapper_CallAsFunction
    Signed-off-by: Peter Nelson <peter@peterdn.com>

--- JSCore/JSCoreObjectWrapper.cpp
+++ JSCore/JSCoreObjectWrapper.cpp
@@ -1,55 +1,122 @@
 #include "stdafx.h"
 
 #include "JSCoreObjectWrapper.h"
+#include "JSCoreMarshal.h"
 
 
 JSClassDefinition wrapperClass = 
 {
-    0,                     /* int version; */
-    kJSClassAttributeNone, /* JSClassAttributes attributes; */
+    0,                       /* int version; */
+    kJSClassAttributeNone,   /* JSClassAttributes attributes; */
 
-    "wrapper",             /* const char* className; */
-    NULL,                  /* JSClassRef parentClass; */
+    "wrapper",               /* const char* className; */
+    NULL,                    /* JSClassRef parentClass; */
         
-    NULL,                  /* const JSStaticValue* staticValues; */
-    NULL,                  /* const JSStaticFunction* staticFunctions; */
+    NULL,                    /* const JSStaticValue* staticValues; */
+    NULL,                    /* const JSStaticFunction* staticFunctions; */
     
-    NULL,                  /* JSObjectInitializeCallback initialize; */
-    NULL,                  /* JSObjectFinalizeCallback finalize; */
-    wrapper_HasProperty,   /* JSObjectHasPropertyCallback hasProperty; */
-    wrapper_GetProperty,   /* JSObjectGetPropertyCallback getProperty; */
-    wrapper_SetProperty    /* JSObjectSetPropertyCallback setProperty; */
-                           /* JSObjectDeletePropertyCallback deleteProperty; */
-                           /* JSObjectGetPropertyNamesCallback getPropertyNames; */
-                           /* JSObjectCallAsFunctionCallback callAsFunction; */
-                           /* JSObjectCallAsConstructorCallback callAsConstructor; */
-                           /* JSObjectHasInstanceCallback hasInstance; */
-                           /* JSObjectConvertToTypeCallback convertToType; */
+    NULL,                    /* JSObjectInitializeCallback initialize; */
+    NULL,                    /* JSObjectFinalizeCallback finalize; */
+    wrapper_HasProperty,     /* JSObjectHasPropertyCallback hasProperty; */
+    wrapper_GetProperty,     /* JSObjectGetPropertyCallback getProperty; */
+    wrapper_SetProperty,     /* JSObjectSetPropertyCallback setProperty; */
+    NULL,                    /* JSObjectDeletePropertyCallback deleteProperty; */
+    NULL,                    /* JSObjectGetPropertyNamesCallback getPropertyNames; */
+    wrapper_CallAsFunction   /* JSObjectCallAsFunctionCallback callAsFunction; */
+                             /* JSObjectCallAsConstructorCallback callAsConstructor; */
+                             /* JSObjectHasInstanceCallback hasInstance; */
+                             /* JSObjectConvertToTypeCallback convertToType; */
 };
+
+Object ^ getObjectFromJSObjectRef(JSObjectRef object)
+{
+    void * ptr = JSObjectGetPrivate(object);
+    GCHandle handle = GCHandle::FromIntPtr(IntPtr(ptr));
+    return handle.Target;
+}
 
 bool wrapper_HasProperty(JSContextRef ctx, JSObjectRef object, JSStringRef propertyName)
 {    
-    void * ptr = JSObjectGetPrivate(object);
-    GCHandle handle = GCHandle::FromIntPtr(IntPtr(ptr));
-    Object ^ obj = handle.Target;
+    Object ^ obj = getObjectFromJSObjectRef(object);
 
-    // TODO: reflection magicks
-
-    return false;
+    String ^ propName = JSCoreMarshal::JSStringToString(propertyName);
+    return obj->GetType()->GetProperty(propName) != nullptr;
 }
 
 JSValueRef wrapper_GetProperty(JSContextRef ctx, JSObjectRef object, JSStringRef propertyName, JSValueRef* exception)
 {
-    void * ptr = JSObjectGetPrivate(object);
-    GCHandle handle = GCHandle::FromIntPtr(IntPtr(ptr));
-    Object ^ obj = handle.Target;
+    Object ^ obj = getObjectFromJSObjectRef(object);
 
-    // TODO: reflection magicks
+    String ^ propName = JSCoreMarshal::JSStringToString(propertyName);
+    PropertyInfo ^ prop = obj->GetType()->GetProperty(propName);
+    if (prop != nullptr)
+    {
+        // for the moment, just return as string
+        if (prop->CanRead)
+        {
+            String ^ val = prop->GetValue(obj, nullptr)->ToString();
+            JSStringRef jsStr = JSCoreMarshal::StringToJSString(val);
+            return JSValueMakeString(ctx, jsStr);
+        }
+        else
+        {
+            // exception?
+        }
+    }
 
     return NULL;
 }
 
-bool wrapper_SetProperty(JSContextRef ctx, JSObjectRef object, JSStringRef propertyName, JSValueRef value, JSValueRef* exception)
+bool wrapper_SetProperty(JSContextRef ctx, JSObjectRef object, 
+                         JSStringRef propertyName, JSValueRef value, JSValueRef* exception)
 {
+    Object ^ obj = getObjectFromJSObjectRef(object);
+
+    String ^ propName = JSCoreMarshal::JSStringToString(propertyName);
+    PropertyInfo ^ prop = obj->GetType()->GetProperty(propName);
+    if (prop != nullptr)
+    {
+        if (prop->CanWrite)
+        {
+            Type ^ type = prop->PropertyType;
+            Object ^ val;
+            JSValueRef exception = NULL;
+
+            if (type == double::typeid)
+            {
+                val = JSValueToNumber(ctx, value, &exception);
+            }
+            else if (type == bool::typeid)
+            {
+                val = JSValueToBoolean(ctx, value);
+            }
+            else if (type == String::typeid)
+            {
+                JSStringRef temp = JSValueToStringCopy(ctx, value, &exception);
+                if (!exception)
+                {
+                    val = JSCoreMarshal::JSStringToString(temp);
+                    JSStringRelease(temp);
+                }
+            }
+
+            if (!exception)
+            {
+                prop->SetValue(obj, val, nullptr);
+                return true;
+            }
+        }
+        else
+        {
+            // exception?
+        }
+    }
+
     return false;
+}
+
+JSValueRef wrapper_CallAsFunction (JSContextRef ctx, JSObjectRef function, JSObjectRef thisObject, 
+                                   size_t argumentCount, const JSValueRef arguments[], JSValueRef* exception)
+{
+    return NULL;
 }
--- JSCore/JSCoreObjectWrapper.h
+++ JSCore/JSCoreObjectWrapper.h
@@ -2,9 +2,13 @@
 
 using namespace System;
 using namespace System::Runtime::InteropServices;
+using namespace System::Reflection;
 
 extern JSClassDefinition wrapperClass;
 
 bool wrapper_HasProperty(JSContextRef ctx, JSObjectRef object, JSStringRef propertyName);
 JSValueRef wrapper_GetProperty(JSContextRef ctx, JSObjectRef object, JSStringRef propertyName, JSValueRef* exception);
-bool wrapper_SetProperty(JSContextRef ctx, JSObjectRef object, JSStringRef propertyName, JSValueRef value, JSValueRef* exception);
+bool wrapper_SetProperty(JSContextRef ctx, JSObjectRef object, JSStringRef propertyName, JSValueRef value, 
+    JSValueRef* exception);
+JSValueRef wrapper_CallAsFunction (JSContextRef ctx, JSObjectRef function, JSObjectRef thisObject, 
+    size_t argumentCount, const JSValueRef arguments[], JSValueRef* exception);
