472393cce490b17c372a15351d8458693762c74d
scampy <scampy@gmail.com>
2010-10-14 13:36:06 -0500

    Adding wrapper_Finalize for freeing up GCHandles created in JSObject

--- JSCore/JSCoreObjectWrapper.h
+++ JSCore/JSCoreObjectWrapper.h
@@ -6,6 +6,7 @@ using namespace System::Reflection;
 
 extern JSClassDefinition wrapperClass;
 
+void wrapper_Finalize(JSObjectRef object);
 bool wrapper_HasProperty(JSContextRef ctx, JSObjectRef object, JSStringRef propertyName);
 JSValueRef wrapper_GetProperty(JSContextRef ctx, JSObjectRef object, JSStringRef propertyName, JSValueRef* exception);
 bool wrapper_SetProperty(JSContextRef ctx, JSObjectRef object, JSStringRef propertyName, JSValueRef value, 
